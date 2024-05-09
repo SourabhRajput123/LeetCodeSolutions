fa6dbd4623458eec687ecbec759ee60f89970453
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-05-09 22:02:39 +0530

    added med prob

--- Medium/2222-number-of-ways-to-select-buildings.py
+++ Medium/2222-number-of-ways-to-select-buildings.py
@@ -21,7 +21,7 @@ class Solution(object):
         
         # Compute the total number of valid selections
         total_ways = 0
-        for i in range(1, n - 1):  # Adjusted iteration range
+        for i in range(1, n - 1):
             if s[i] == '0':
                 total_ways += n01[i - 1] + n10[i - 1]
             elif s[i] == '1':
