08cb1938bf33c2b3d40696106a6d63d8223bb9f9
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-05-10 20:10:53 +0530

    added med prob

---
+++ Medium/2353-design-a-food-rating-system.py
@@ -0,0 +1,30 @@
+class FoodRatings(object):
+
+    def __init__(self, foods, cuisines, ratings):
+        self.food_ratings = {}  # Dictionary to store ratings for each food
+        self.highest_rated_per_cuisine = {}  # Dictionary to store highest rated food for each cuisine
+        
+        # Initialize food ratings
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_ratings[food] = rating
+            
+            # Update highest rated food for each cuisine
+            if cuisine not in self.highest_rated_per_cuisine or self.food_ratings[self.highest_rated_per_cuisine[cuisine]] < rating:
+                self.highest_rated_per_cuisine[cuisine] = food
+                
+    def changeRating(self, food, newRating):
+        # Update rating for the given food
+        self.food_ratings[food] = newRating
+        
+        # Update highest rated food for its cuisine if necessary
+        cuisine = self.get_cuisine(food)
+        if self.food_ratings[self.highest_rated_per_cuisine[cuisine]] < newRating:
+            self.highest_rated_per_cuisine[cuisine] = food
+
+    def highestRated(self, cuisine):
+        # Return the highest rated food for the given cuisine
+        return self.highest_rated_per_cuisine[cuisine]
+
+    def get_cuisine(self, food):
+        # Helper function to get the cuisine of a given food
+        return [cuisine for cuisine, f in self.highest_rated_per_cuisine.items() if f == food][0]
