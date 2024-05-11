4c678b7abd7ea95baea39104c392a996e29b8369
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-05-11 21:55:41 +0530

    added med prob

---
+++ Medium/300-longest-increasing-subsequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        dp = [1] * n  # Initialize dp array with 1 as the default length
+        
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        return max(dp)
