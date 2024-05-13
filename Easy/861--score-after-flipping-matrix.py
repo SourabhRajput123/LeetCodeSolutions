3e242e3150dcaf7e6cb83b9336f7c4a4f855eba3
Sourabh Singh Rajput <sourabhr684@gmail.com>
2024-05-13 20:57:30 +0530

    added med prob

--- Easy/861--score-after-flipping-matrix.py
+++ Easy/861--score-after-flipping-matrix.py
@@ -1,33 +1,30 @@
-def toggle_bit(num):
-    return 1 - num
+class Solution(object):
+    def toggle_bit(self, num):
+        return 1 - num
 
-def matrix_score(grid):
-    m, n = len(grid), len(grid[0])
+    def matrixScore(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        m, n = len(grid), len(grid[0])
 
-    # Step 1: Ensure first bit of every row is 1
-    for i in range(m):
-        if grid[i][0] == 0:
-            for j in range(n):
-                grid[i][j] = toggle_bit(grid[i][j])
+        # Step 1: Ensure first bit of every row is 1
+        for i in range(m):
+            if grid[i][0] == 0:
+                for j in range(n):
+                    grid[i][j] = self.toggle_bit(grid[i][j])
 
-    # Step 2: Maximize number of 1s in each column
-    for j in range(1, n):
-        count_ones = sum(grid[i][j] for i in range(m))
-        if count_ones < m - count_ones:
-            for i in range(m):
-                grid[i][j] = toggle_bit(grid[i][j])
+        # Step 2: Maximize number of 1s in each column
+        for j in range(1, n):
+            count_ones = sum(grid[i][j] for i in range(m))
+            if count_ones < m - count_ones:
+                for i in range(m):
+                    grid[i][j] = self.toggle_bit(grid[i][j])
 
-    # Calculate the score
-    score = 0
-    for row in grid:
-        score += int(''.join(map(str, row)), 2)
+        # Calculate the score
+        score = 0
+        for row in grid:
+            score += int(''.join(map(str, row)), 2)
 
-    return score
-
-# Example usage:
-grid = [
-    [0, 0, 1, 1],
-    [1, 0, 1, 0],
-    [0, 1, 1, 0]
-]
-print(matrix_score(grid))  # Output should be 39
+        return score
